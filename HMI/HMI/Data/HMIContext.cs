8edf9fc0dae8cdddcef54139385b90d14dd66607
Thomas <Thomas.joon.nielsen@consultant.lego.com>
2021-06-21 20:19:58 +0200

    [HMI-4] Updating packages and scaffolding dbcontext to create db

---
+++ HMI/HMI/Data/HMIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HMI.Models;
+
+namespace HMI.Data
+{
+    public class HMIContext : DbContext
+    {
+        public HMIContext (DbContextOptions<HMIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HMI.Models.Names> Names { get; set; }
+    }
+}
---
+++ HMI/HMI/Data/NamesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HMI.Models;
+
+namespace HMI.Data
+{
+    public class NamesController : Controller
+    {
+        private readonly HMIContext _context;
+
+        public NamesController(HMIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Names
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Names.ToListAsync());
+        }
+
+        // GET: Names/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var names = await _context.Names
+                .FirstOrDefaultAsync(m => m.NameID == id);
+            if (names == null)
+            {
+                return NotFound();
+            }
+
+            return View(names);
+        }
+
+        // GET: Names/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Names/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NameID,Name,Address")] Names names)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(names);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(names);
+        }
+
+        // GET: Names/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var names = await _context.Names.FindAsync(id);
+            if (names == null)
+            {
+                return NotFound();
+            }
+            return View(names);
+        }
+
+        // POST: Names/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("NameID,Name,Address")] Names names)
+        {
+            if (id != names.NameID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(names);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NamesExists(names.NameID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(names);
+        }
+
+        // GET: Names/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var names = await _context.Names
+                .FirstOrDefaultAsync(m => m.NameID == id);
+            if (names == null)
+            {
+                return NotFound();
+            }
+
+            return View(names);
+        }
+
+        // POST: Names/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var names = await _context.Names.FindAsync(id);
+            _context.Names.Remove(names);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NamesExists(string id)
+        {
+            return _context.Names.Any(e => e.NameID == id);
+        }
+    }
+}

610bd08f407b9496abc715a0ae856eb26a6bc07d
Thomas <Thomas.joon.nielsen@consultant.lego.com>
2021-06-21 19:26:29 +0200

    [HMI-2] Create Names entity model

---
+++ HMI/HMI/Models/Names.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMI.Models
+{
+    public class Names
+    {
+        public string NameID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
